9ebaf125507b98b1e80e62ec697841dc9e16cd51
kalil0321 <kalil.bouzigues@gmail.com>
2025-05-29 20:46:14 +0200

    update mode-toggle.tsx

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -1,29 +1,48 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { useTheme } from "next-themes"
+import * as React from "react";
+import { useTheme } from "next-themes";
 
-import { Moon, Sun } from "lucide-react"
-import { Button } from "@/components/ui/button"
+import { Moon, Sun } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 export function ModeToggle() {
-  const { setTheme, resolvedTheme } = useTheme()
-  const [, startTransition] = React.useTransition()
+    const { setTheme, resolvedTheme } = useTheme();
+    const [, startTransition] = React.useTransition();
+    const [mounted, setMounted] = React.useState(false);
 
-  return (
-    <Button
-      className="h-7 w-7"
-      onClick={() => {
-        startTransition(() => {
-          setTheme(resolvedTheme === "dark" ? "light" : "dark")
-        })
-      }}
-      size="icon"
-      variant="ghost"
-    >
-      <Moon className="dark:hidden" />
-      <Sun className="hidden dark:block" />
-      <span className="sr-only">Toggle theme</span>
-    </Button>
-  )
+    React.useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    if (!mounted) {
+        return (
+            <Button
+                className="h-7 w-7"
+                size="icon"
+                variant="ghost"
+                suppressHydrationWarning
+            >
+                <Sun className="h-[1.2rem] w-[1.2rem]" />
+                <span className="sr-only">Toggle theme</span>
+            </Button>
+        );
+    }
+
+    return (
+        <Button
+            className="h-7 w-7"
+            onClick={() => {
+                startTransition(() => {
+                    setTheme(resolvedTheme === "dark" ? "light" : "dark");
+                });
+            }}
+            size="icon"
+            variant="ghost"
+        >
+            <Moon className="dark:hidden" />
+            <Sun className="hidden dark:block" />
+            <span className="sr-only">Toggle theme</span>
+        </Button>
+    );
 }
