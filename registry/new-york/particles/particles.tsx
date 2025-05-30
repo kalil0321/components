a933a32b0ed25a99a6fe2fdfe61ab4df66be7e64
kalil0321 <kalil.bouzigues@gmail.com>
2025-05-30 17:20:38 +0200

    update particles

--- registry/new-york/particles/particles.tsx
+++ registry/new-york/particles/particles.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useTheme } from "next-themes";
 import * as React from "react";
 import ParticleImage, {
     ParticleForce,
@@ -7,21 +8,10 @@ import ParticleImage, {
     forces,
 } from "react-particle-image";
 
-const darkModeParticleOptions: ParticleOptions = {
+const particleOptions: ParticleOptions = {
     color: ({ x, y, image }) => {
-        // Get pixel
         const pixel = image.get(x, y);
-        // Return the color of the pixel
         return `rgb(${pixel.r}, ${pixel.g}, ${pixel.b})`;
-    },
-};
-
-const lightModeParticleOptions: ParticleOptions = {
-    color: ({ x, y, image }) => {
-        // Get pixel
-        const pixel = image.get(x, y);
-        // Invert colors - make white pixels black and black pixels white
-        return `rgb(${255 - pixel.r}, ${255 - pixel.g}, ${255 - pixel.b})`;
     },
 };
 
@@ -34,47 +24,36 @@ const motionForceDown = (x: number, y: number): ParticleForce => {
 };
 
 export default function Particles({
-    image = "https://github.com/github.png",
+    image = "https://avatars.githubusercontent.com/u/92564081",
+    maxParticles = 10_000,
+    width = 500,
+    height = 500,
+    scale = 1,
+    backgroundColor,
 }: {
     image: string;
+    maxParticles: number;
+    width: number;
+    height: number;
+    scale: number;
+    backgroundColor: string;
 }) {
-    const [isDarkMode, setIsDarkMode] = React.useState(false);
+    const { setTheme, resolvedTheme } = useTheme();
 
-    React.useEffect(() => {
-        // Check if window.matchMedia is available (client-side only)
-        if (typeof window !== "undefined") {
-            // Get initial system preference
-            const darkModeQuery = window.matchMedia(
-                "(prefers-color-scheme: dark)"
-            );
-            setIsDarkMode(darkModeQuery.matches);
+    const bgColor =
+        backgroundColor || (resolvedTheme === "dark" ? "transparent" : "black");
 
-            // Listen for changes in system preference
-            const handler = (e: MediaQueryListEvent) =>
-                setIsDarkMode(e.matches);
-            darkModeQuery.addEventListener("change", handler);
-
-            // Cleanup
-            return () => darkModeQuery.removeEventListener("change", handler);
-        }
-    }, []);
-
-    const scale = 1 / 3;
     return (
         <div className="justify-center items-center p-4">
             <ParticleImage
                 src={image}
                 scale={scale}
                 entropy={30}
-                width={100 * scale}
-                height={100 * scale}
-                maxParticles={20_000}
-                particleOptions={
-                    isDarkMode
-                        ? darkModeParticleOptions
-                        : lightModeParticleOptions
-                }
-                backgroundColor="black"
+                width={width * scale}
+                height={height * scale}
+                maxParticles={maxParticles}
+                particleOptions={particleOptions}
+                backgroundColor={bgColor}
                 mouseMoveForce={motionForce}
                 mouseDownForce={motionForceDown}
             />
